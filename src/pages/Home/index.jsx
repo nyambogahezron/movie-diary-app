08ebb22a4d255d6fd12f837c3601301f65b10d7d
Hezron Nyamboga <hezzyn1@gmail.com>
2023-11-25 16:08:19 +0300

    not found page added

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import Navbar from '../../components/Navbar';
-import {FaHeart, FaStar, FaStarHalfAlt} from 'react-icons/fa'
+import { FaHeart, FaStar, FaStarHalfAlt } from 'react-icons/fa';
 
 const Home = () => {
   return (
@@ -18,99 +18,178 @@ const Home = () => {
               <div className='list-card-img'>
                 <img src='movie.jpeg' alt='' />
               </div>
-             <div className='list-desp'>
+              <div className='list-description'>
                 <div className='list-card-title'>Wednesday</div>
-              <div className='list-card-sn'>Sn 2/5</div>
-              <div className='list-card-rate'> <FaStar/> <FaStar/><FaStar/><FaStar/><FaStarHalfAlt /></div>
-             </div>
-              <div className='list-card-favorite'><FaHeart className="FaHeart"/></div>
-              <div className='list-card-favorite-active'><FaHeart className="favorite-active"/></div>
+                <div className='list-card-sn'>Sn 2/5</div>
+                <div className='list-card-rate'>
+                  {' '}
+                  <FaStar /> <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStarHalfAlt />
+                </div>
+              </div>
+              <div className='list-card-favorite'>
+                <FaHeart className='FaHeart' />
+              </div>
+              <div className='list-card-favorite-active'>
+                <FaHeart className='favorite-active' />
+              </div>
             </div>
-                 <div className='list-card'>
+            <div className='list-card'>
               <div className='list-card-img'>
                 <img src='movie.jpeg' alt='' />
               </div>
-             <div className='list-desp'>
+              <div className='list-description'>
                 <div className='list-card-title'>Wednesday</div>
-              <div className='list-card-sn'>Sn 2/5</div>
-              <div className='list-card-rate'> <FaStar/> <FaStar/><FaStar/><FaStar/><FaStarHalfAlt /></div>
-             </div>
-              <div className='list-card-favorite'><FaHeart className="FaHeart"/></div>
-              <div className='list-card-favorite-active'><FaHeart className="favorite-active"/></div>
+                <div className='list-card-sn'>Sn 2/5</div>
+                <div className='list-card-rate'>
+                  {' '}
+                  <FaStar /> <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStarHalfAlt />
+                </div>
+              </div>
+              <div className='list-card-favorite'>
+                <FaHeart className='FaHeart' />
+              </div>
+              <div className='list-card-favorite-active'>
+                <FaHeart className='favorite-active' />
+              </div>
             </div>
-                 <div className='list-card'>
+            <div className='list-card'>
               <div className='list-card-img'>
                 <img src='movie.jpeg' alt='' />
               </div>
-             <div className='list-desp'>
+              <div className='list-description'>
                 <div className='list-card-title'>Wednesday</div>
-              <div className='list-card-sn'>Sn 2/5</div>
-              <div className='list-card-rate'> <FaStar/> <FaStar/><FaStar/><FaStar/><FaStarHalfAlt /></div>
-             </div>
-              <div className='list-card-favorite'><FaHeart className="FaHeart"/></div>
-              <div className='list-card-favorite-active'><FaHeart className="favorite-active"/></div>
+                <div className='list-card-sn'>Sn 2/5</div>
+                <div className='list-card-rate'>
+                  {' '}
+                  <FaStar /> <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStarHalfAlt />
+                </div>
+              </div>
+              <div className='list-card-favorite'>
+                <FaHeart className='FaHeart' />
+              </div>
+              <div className='list-card-favorite-active'>
+                <FaHeart className='favorite-active' />
+              </div>
             </div>
-                 <div className='list-card'>
+            <div className='list-card'>
               <div className='list-card-img'>
                 <img src='movie.jpeg' alt='' />
               </div>
-             <div className='list-desp'>
+              <div className='list-description'>
                 <div className='list-card-title'>Wednesday</div>
-              <div className='list-card-sn'>Sn 2/5</div>
-              <div className='list-card-rate'> <FaStar/> <FaStar/><FaStar/><FaStar/><FaStarHalfAlt /></div>
-             </div>
-              <div className='list-card-favorite'><FaHeart className="FaHeart"/></div>
-              <div className='list-card-favorite-active'><FaHeart className="favorite-active"/></div>
+                <div className='list-card-sn'>Sn 2/5</div>
+                <div className='list-card-rate'>
+                  {' '}
+                  <FaStar /> <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStarHalfAlt />
+                </div>
+              </div>
+              <div className='list-card-favorite'>
+                <FaHeart className='FaHeart' />
+              </div>
+              <div className='list-card-favorite-active'>
+                <FaHeart className='favorite-active' />
+              </div>
             </div>
-                 <div className='list-card'>
+            <div className='list-card'>
               <div className='list-card-img'>
                 <img src='movie.jpeg' alt='' />
               </div>
-             <div className='list-desp'>
+              <div className='list-description'>
                 <div className='list-card-title'>Wednesday</div>
-              <div className='list-card-sn'>Sn 2/5</div>
-              <div className='list-card-rate'> <FaStar/> <FaStar/><FaStar/><FaStar/><FaStarHalfAlt /></div>
-             </div>
-              <div className='list-card-favorite'><FaHeart className="FaHeart"/></div>
-              <div className='list-card-favorite-active'><FaHeart className="favorite-active"/></div>
+                <div className='list-card-sn'>Sn 2/5</div>
+                <div className='list-card-rate'>
+                  {' '}
+                  <FaStar /> <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStarHalfAlt />
+                </div>
+              </div>
+              <div className='list-card-favorite'>
+                <FaHeart className='FaHeart' />
+              </div>
+              <div className='list-card-favorite-active'>
+                <FaHeart className='favorite-active' />
+              </div>
             </div>
-                 <div className='list-card'>
+            <div className='list-card'>
               <div className='list-card-img'>
                 <img src='movie.jpeg' alt='' />
               </div>
-             <div className='list-desp'>
+              <div className='list-description'>
                 <div className='list-card-title'>Wednesday</div>
-              <div className='list-card-sn'>Sn 2/5</div>
-              <div className='list-card-rate'> <FaStar/> <FaStar/><FaStar/><FaStar/><FaStarHalfAlt /></div>
-             </div>
-              <div className='list-card-favorite'><FaHeart className="FaHeart"/></div>
-              <div className='list-card-favorite-active'><FaHeart className="favorite-active"/></div>
+                <div className='list-card-sn'>Sn 2/5</div>
+                <div className='list-card-rate'>
+                  {' '}
+                  <FaStar /> <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStarHalfAlt />
+                </div>
+              </div>
+              <div className='list-card-favorite'>
+                <FaHeart className='FaHeart' />
+              </div>
+              <div className='list-card-favorite-active'>
+                <FaHeart className='favorite-active' />
+              </div>
             </div>
-                 <div className='list-card'>
+            <div className='list-card'>
               <div className='list-card-img'>
                 <img src='movie.jpeg' alt='' />
               </div>
-             <div className='list-desp'>
+              <div className='list-description'>
                 <div className='list-card-title'>Wednesday</div>
-              <div className='list-card-sn'>Sn 2/5</div>
-              <div className='list-card-rate'> <FaStar/> <FaStar/><FaStar/><FaStar/><FaStarHalfAlt /></div>
-             </div>
-              <div className='list-card-favorite'><FaHeart className="FaHeart"/></div>
-              <div className='list-card-favorite-active'><FaHeart className="favorite-active"/></div>
+                <div className='list-card-sn'>Sn 2/5</div>
+                <div className='list-card-rate'>
+                  {' '}
+                  <FaStar /> <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStarHalfAlt />
+                </div>
+              </div>
+              <div className='list-card-favorite'>
+                <FaHeart className='FaHeart' />
+              </div>
+              <div className='list-card-favorite-active'>
+                <FaHeart className='favorite-active' />
+              </div>
             </div>
-                 <div className='list-card'>
+            <div className='list-card'>
               <div className='list-card-img'>
                 <img src='movie.jpeg' alt='' />
               </div>
-             <div className='list-desp'>
+              <div className='list-description'>
                 <div className='list-card-title'>Wednesday</div>
-              <div className='list-card-sn'>Sn 2/5</div>
-              <div className='list-card-rate'> <FaStar/> <FaStar/><FaStar/><FaStar/><FaStarHalfAlt /></div>
-             </div>
-              <div className='list-card-favorite'><FaHeart className="FaHeart"/></div>
-              <div className='list-card-favorite-active'><FaHeart className="favorite-active"/></div>
+                <div className='list-card-sn'>Sn 2/5</div>
+                <div className='list-card-rate'>
+                  {' '}
+                  <FaStar /> <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStarHalfAlt />
+                </div>
+              </div>
+              <div className='list-card-favorite'>
+                <FaHeart className='FaHeart' />
+              </div>
+              <div className='list-card-favorite-active'>
+                <FaHeart className='favorite-active' />
+              </div>
             </div>
-
           </article>
         </div>
       </main>
--- src/pages/utilities/NotFound.jsx
+++ src/pages/utilities/NotFound.jsx
@@ -2,10 +2,15 @@ import { Link } from 'react-router-dom';
 const NotFound = () => {
   return (
     <div className='not-found-container'>
-      <h1>Sorry, the page you were looking for was not found.</h1>
-      <Link to='/' className='link-button'>
-        Return to Home
+    <div className='NotFound-info'>
+      <div className='status-code'>404</div>
+      <div className='title'>PAGE NOT FOUND :)</div>
+  <Link to='/' className='link-button btn'>
+        Home
       </Link>
+
+    </div>
+      
     </div>
   );
 };
