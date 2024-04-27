e5f4f285d7ec9d060b82e16b17b3133a5c75f626
Hezron Nyamboga <hezzyn1@gmail.com>
2024-04-27 04:02:28 +0300

    update login page ui

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,18 +1,18 @@
-import { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { Alert } from '../../components/Alert';
-import { useDispatch, useSelector} from 'react-redux';
-import {useLoginMutation} from '../../slices/usersApiSlice';
-import { setCredentials} from '../../slices/authSlice';
-import { toast } from 'react-toastify';
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useLoginMutation } from "../../slices/usersApiSlice";
+import { setCredentials } from "../../slices/authSlice";
+import { toast } from "react-toastify";
+import logo from "../../assets/logo.png";
 
 const Login = () => {
-  const [userName, setUserName] = useState('');
-  const [password, setPassword] = useState('');
-  const [rememberMe, setRememberMe] = useState('');
-  const [isError, setIsError] = useState(false);
-  const [showAlert, setShowAlert] = useState(false);
-  const [alertMsg, setAlertMsg] = useState('');
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState("");
+  // const [isError, setIsError] = useState(false);
+  // const [showAlert, setShowAlert] = useState(false);
+  // const [alertMsg, setAlertMsg] = useState("");
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -23,83 +23,87 @@ const Login = () => {
 
   useEffect(() => {
     if (userInfo) {
-      navigate('/');
+      navigate("/");
     }
   }, [navigate, userInfo]);
-
 
   const handleLoginSubmit = async (e) => {
     e.preventDefault();
     console.log(userName, password, rememberMe);
     {
       if (!userName || !password) {
-        toast.error("Please provide all fields")
+        toast.error("Please provide all fields");
       } else {
         try {
-      const res = await login({ email, password }).unwrap();
-      dispatch(setCredentials({ ...res }));
-      navigate('/');
-    } catch (err) {
-      toast.error(err?.data?.message || err.error);
-    }
+          const res = await login({ userName, password }).unwrap();
+          dispatch(setCredentials({ ...res }));
+          navigate("/");
+        } catch (err) {
+          toast.error(err?.data?.message || err.error);
+        }
       }
     }
   };
 
   return (
     <main className='login'>
-      <div className='title form-title'>Login</div>
-      <div className={`error  ${showAlert ? 'show' : 'hidden'}`}>
-        <Alert alertMsg={alertMsg} isError={isError} />
+      <div className='logo'>
+        <span className='logo'>
+          <img src={logo} alt='logo' />
+        </span>
       </div>
-
-      <form className='form login-form ' onSubmit={handleLoginSubmit}>
-        <div className='form-row'>
-          <label htmlFor='username' className='form-label'>
-            Username/Email
-          </label>
-          <input
-            type='text'
-            className='form-input username-input'
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-            id='userName'
-          />
-        </div>
-        <div className='form-row'>
-          <label htmlFor='password' className='form-label'>
-            Password
-          </label>
-          <input
-            type='password'
-            className='form-input password-input'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            id='password'
-          />
-        </div>
-        <div className='form-row' style={{ textAlign: 'left' }}>
-          <input
-            type='checkbox'
-            checked={rememberMe}
-            id='rememberMe'
-            name='rememberMe'
-            onChange={(e) => setRememberMe(e.target.checked)}
-          />
-          <label htmlFor='rememberMe'> Remember Me </label>
-        </div>
-        <button type='submit' className='submit-btn btn btn-block'>
-          Login
-        </button>
-      </form>
-
-      <div className='register-sec'>
-        <div className='reg-txt'>
-          <hr />
-          {`Don't Have Account`} <hr />
-        </div>
-        <div className='nav-user-btn'>
-          <Link to='/register'>Register</Link>
+      <div className='form-wrapper'>
+        <div className='title form-title'>Login</div>
+        <form className='form login-form ' onSubmit={handleLoginSubmit}>
+          <div className='form-row'>
+            <label htmlFor='username' className='form-label'>
+              Username/Email
+            </label>
+            <input
+              type='text'
+              className='form-input username-input'
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              id='userName'
+            />
+          </div>
+          <div className='form-row'>
+            <label htmlFor='password' className='form-label'>
+              Password
+            </label>
+            <input
+              type='password'
+              className='form-input password-input'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              id='password'
+            />
+          </div>
+          <div className='form-row rem-me-input' style={{ textAlign: "left" }}>
+            <input
+              type='checkbox'
+              checked={rememberMe}
+              id='rememberMe'
+              name='rememberMe'
+              onChange={(e) => setRememberMe(e.target.checked)}
+            />
+            <label htmlFor='rememberMe' className='rem-txt'>
+              {" "}
+              Remember Me{" "}
+            </label>
+          </div>
+          <button type='submit' className='submit-btn btn btn-block'>
+            Login
+          </button>
+        </form>
+        <div className='register-sec'>
+          <div className='reg-txt'>
+            <hr />
+            {`Don't Have Account`} <hr />
+          </div>
+          <div className='nav-user-btn'>
+            <Link to='/register'>Register</Link>
+          </div>
         </div>
       </div>
     </main>
